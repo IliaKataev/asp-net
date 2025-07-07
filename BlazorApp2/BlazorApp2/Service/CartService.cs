86517503540bb0962b185f24362f1987afe4c50c
IliaKataev <163750267+IliaKataev@users.noreply.github.com>
2025-07-07 20:23:12 +0500

    blazor addition

---
+++ BlazorApp2/BlazorApp2/Service/CartService.cs
@@ -0,0 +1,26 @@
+﻿using BlazorApp2.Shared;
+
+namespace BlazorApp2.Service
+{
+    public class CartService
+    {
+        private readonly List<Product> cartItems = new();
+
+        public IReadOnlyList<Product> GetCartItems() => cartItems;
+
+        public void AddToCart(Product product)
+        {
+            cartItems.Add(product);
+        }
+
+        public void RemoveFromCart(Product product)
+        {
+            cartItems.Remove(product);
+        }
+
+        public void ClearCart()
+        {
+            cartItems.Clear();
+        }
+    }
+}
