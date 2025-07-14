bbf9523d820ca6ace0ac10fd24ad30f544b40e89
IliaKataev <163750267+IliaKataev@users.noreply.github.com>
2025-07-14 21:36:39 +0500

    ToDoList

---
+++ ToDoList/ToDoList/Model/TaskModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoList.Model
+{
+    public class TaskModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
