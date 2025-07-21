e32cb6cf6e8c683480158349813b69b93c6c4afb
IliaKataev <163750267+IliaKataev@users.noreply.github.com>
2025-07-21 20:49:37 +0500

    delete edit iscompleted

--- ToDoList/ToDoList/Model/TaskModel.cs
+++ ToDoList/ToDoList/Model/TaskModel.cs
@@ -2,6 +2,12 @@
 using ToDoList.Pages;
 namespace ToDoList.Model
 {
+    public enum TaskPriority
+    {
+        Low,
+        Medium,
+        High
+    }
     public class TaskModel
     {
         public Guid Id { get; set; } = Guid.NewGuid(); // sdfgjhkdfjkhshdkjfhbdsf
@@ -14,6 +20,12 @@ namespace ToDoList.Model
 
         [Range(typeof(DateTime), "01/01/2020", "01/01/2030", ErrorMessage = "Веберите год между 20 и 30")]
         [NoWeekendDate(ErrorMessage = "Выберите будние дни")]
+        [NoPastDate(ErrorMessage = "Дата не может быть меньше текущей")]
         public DateTime DueDate { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = "Выберите приоритет")]
+        public TaskPriority Priority { get; set; } = TaskPriority.Low;
+
+        public bool IsCompleted { get; set; } = false;
     }
 }
---
+++ ToDoList/ToDoList/Pages/NoPastDateAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace ToDoList.Pages
+{
+    public class NoPastDateAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+            if (value is DateTime date)
+            {
+                return date.Date >= DateTime.Today;
+            }
+
+            return true;
+        }
+    }
+
+}
